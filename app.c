23cce225555ab1ad19bc86828538868ed5887d55
Aditya Dhar <adityadhar2003@gmail.com>
2026-08-15 15:32:23 +0530

    Initial commits

--- app.c
+++ app.c
@@ -3,7 +3,7 @@
 
 #define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
-#incldue <SDL3/SDL_main.h>
+#include <SDL3/SDL_main.h>
 
 #include "config.h"
 
@@ -17,8 +17,24 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]){
 		,WINDOW_WIDTH
 		,WINDOW_HEIGHT
 		,WINDOW_FLAG
-		,window
-		,renderer
+		,&window
+		,&renderer
 	);
-	return SDL_APP_CONTINUE
+	return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event){
+	if(event->type == SDL_EVENT_QUIT) return SDL_APP_SUCCESS;
+	return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate){
+	SDL_SetRenderDrawColor(renderer, 55, 66, 66, SDL_ALPHA_OPAQUE);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+	return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result){
+
 }
